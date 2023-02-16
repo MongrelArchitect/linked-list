21dc3835891a9274aafb81e2a1e0774d15a3f312
Sean Eric Thomas <set@seanericthomas.com>
2023-02-16 14:37:04 -0800

    Keep track of node quantity

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ const Node = (data, next = null) => ({ data, next });
 
 const LinkedList = () => {
   let head = null;
+  let size = 0;
 
   const getHead = () => head;
+
+  const getSize = () => size;
 
   const append = (value) => {
     const newNode = Node(value);
     if (!head) {
       // We've got an empty list, so we're appending the first node
       head = newNode;
+      size += 1;
     } else {
       // Traverse the list until the end and append there
       let currentNode = head;
@@ -17,6 +21,7 @@ const LinkedList = () => {
         currentNode = currentNode.next;
       }
       currentNode.next = newNode;
+      size += 1;
     }
   };
 
@@ -25,9 +30,12 @@ const LinkedList = () => {
     const newNode = Node(value);
     newNode.next = head;
     head = newNode;
+    size += 1;
   };
 
-  return { append, prepend, getHead };
+  return {
+    getSize, append, prepend, getHead,
+  };
 };
 
 const list = LinkedList();
